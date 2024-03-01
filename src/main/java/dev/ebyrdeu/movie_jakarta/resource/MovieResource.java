91c26692754596c64efa5ba0f2a86b87bd90d4f4
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-01 13:15:30 +0100

    Add endpoints POST, PUT,DELETE,GET

--- src/main/java/dev/ebyrdeu/movie_jakarta/resource/MovieResource.java
+++ src/main/java/dev/ebyrdeu/movie_jakarta/resource/MovieResource.java
@@ -1,14 +1,43 @@
 package dev.ebyrdeu.movie_jakarta.resource;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import dev.ebyrdeu.movie_jakarta.dto.MovieDto;
+import dev.ebyrdeu.movie_jakarta.dto.Movies;
+import dev.ebyrdeu.movie_jakarta.entity.Movie;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello-world")
+import java.net.URI;
+
+@Path("/movies")
 public class MovieResource {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public Response all() {
+        return Response.ok().build();
     }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(MovieDto movieDto){
+        Movie movie = MovieDto.map(movieDto);
+        // movieRepository.update(movie)
+        return Response.ok(movie).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(MovieDto movieDto){
+        //MovieRepository.add()
+        return Response.created(URI.create("http://localhost:8080")).build();
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response delete(MovieDto movieDto){
+        Movie movie = MovieDto.map(movieDto);
+        //movieRepository.delete();
+        return Response.noContent().build();
+    }
+
 }
