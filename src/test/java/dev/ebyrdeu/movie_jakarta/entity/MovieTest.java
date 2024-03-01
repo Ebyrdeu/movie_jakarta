2c36c7d32a6199e926d3e227029aa24c8a8edefb
xsukva <v.trana@icloud.com>
2024-03-01 15:45:44 +0100

    preparation of MovieTest for Entity class Movie

---
+++ src/test/java/dev/ebyrdeu/movie_jakarta/entity/MovieTest.java
@@ -0,0 +1,107 @@
+package dev.ebyrdeu.movie_jakarta.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.UUID;
+
+class MovieTest {
+    private Movie movie;
+
+    @BeforeEach
+    void setUp() {
+        movie = new Movie();
+    }
+
+    @Test
+    @DisplayName("getId return correct Id assigned with setId method")
+    void getIdReturnCorrectIdAssignedWithSetIdMethod() {
+        UUID id = UUID.randomUUID();
+        movie.setId(id);
+
+        assertEquals(id, movie.getId());
+    }
+
+    @Test
+    @DisplayName("getTitle return correct title assigned with setTitle method")
+    void getTitleReturnCorrectTitleAssignedWithSetTitleMethod() {
+        movie.setTitle("Roma");
+
+        assertEquals("Roma", movie.getTitle());
+    }
+
+    @Test
+    @DisplayName("getReleaseYear method return correct Year assigned with setRelese method")
+    void getReleaseYearMethodReturnCorrectYearAssignedWithSetReleseMethod() {
+        movie.setReleaseYear(1972);
+
+        assertEquals(1972, movie.getReleaseYear());
+    }
+
+    @Test
+    @DisplayName("getDirector method return correct Director assigned with setDirector Method")
+    void getDirectorMethodReturnCorrectDirectorAssignedWithSetDirectorMethod() {
+        movie.setDirector("Federico Fellini");
+
+        assertEquals("Federico Fellini", movie.getDirector());
+    }
+
+    @Test
+    @DisplayName("Movies with same id return Equals")
+    void MoviesWithSameIdReturnEqual() {
+        UUID id1 = UUID.randomUUID();
+
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+
+        movie1.setId(id1);
+        movie2.setId(id1); // same id as movie1
+
+        assertEquals(movie1, movie2);
+    }
+
+    @Test
+    @DisplayName("Movies with different id return NotEquals")
+    void MoviesWithDifferentIdReturnNotEqual() {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+
+        Movie movie1 = new Movie();
+        Movie movie3 = new Movie();
+
+        movie1.setId(id1);
+        movie3.setId(id2); // id different from movie1
+
+        assertNotEquals(movie1, movie3);
+    }
+
+    @Test
+    void testHashCode() {
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+
+        assertEquals(movie1.hashCode(), movie2.hashCode());
+    }
+ // to delete
+    @Test
+    @DisplayName("equal UUID return")
+    void equalUuidReturn() {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+        Movie movie3 = new Movie();
+
+        movie1.setId(id1);
+        movie2.setId(id1); // same id as movie1
+        movie3.setId(id2); // id different from movie1
+
+        assertTrue(movie1.equals(movie2));
+        assertFalse(movie1.equals(movie3));
+
+        assertFalse(movie1.equals(null));
+    }
+
+}
