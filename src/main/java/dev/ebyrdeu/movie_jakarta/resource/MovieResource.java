37c7c416598e08fe7eba60697d778b7de0ea9529
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-03 18:28:41 +0100

    Add Validation

--- src/main/java/dev/ebyrdeu/movie_jakarta/resource/MovieResource.java
+++ src/main/java/dev/ebyrdeu/movie_jakarta/resource/MovieResource.java
@@ -5,7 +5,9 @@ import dev.ebyrdeu.movie_jakarta.dto.Movies;
 import dev.ebyrdeu.movie_jakarta.entity.Movie;
 import dev.ebyrdeu.movie_jakarta.repository.MovieRepository;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.EntityTag;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -19,11 +21,11 @@ public class MovieResource {
 
     private MovieRepository movieRepository;
 
-    public MovieResource(){
+    public MovieResource() {
     }
 
     @Inject
-    public MovieResource(MovieRepository movieRepository){
+    public MovieResource(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
@@ -36,7 +38,7 @@ public class MovieResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response one(@PathParam("id") UUID id){
+    public Response one(@PathParam("id") @NotNull UUID id) {
         Optional<Movie> optionalMovie = movieRepository.findById(id);
         if (optionalMovie.isPresent()) {
             MovieDto movieDto = MovieDto.map(optionalMovie.get());
@@ -48,24 +50,27 @@ public class MovieResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response add(MovieDto movieDto){
+    public Response add(@NotNull MovieDto movieDto) {
         Movie movie = movieRepository.saveMovie(MovieDto.map(movieDto));
-        return Response.created(URI.create("http://localhost:8080/app/api/movies" + movie.getId())).build();
+        return Response.created(URI.create("http://localhost:8080/app/api/movies/" + movie.getId())).build();
     }
 
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response update(@PathParam("id")UUID id,MovieDto movieDetails){
+    public Response update(@PathParam("id") @NotNull UUID id, @NotNull MovieDto movieDetails) {
         Movie movie = movieRepository.updateMovie(id, MovieDto.map(movieDetails));
         return Response.ok(movie).build();
     }
 
     @DELETE
     @Path("/{id}")
-    public Response delete(@PathParam("id") UUID id){
-        movieRepository.deleteMovie(id);
+    public Response delete(@PathParam("id") @NotNull UUID id) {
+        try {
+            movieRepository.deleteMovie(id);
+        } catch (NotFoundException notFound) {
+            return Response.notModified(new EntityTag("Id: " + id + " does not exists")).build();
+        }
         return Response.noContent().build();
     }
 }
